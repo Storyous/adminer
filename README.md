356798a42ac487710a05a0b306fdeb121d6ef19e
Ivo Šofránek <ivo.sofranek@teya.com>
2025-03-18 13:59:30 +0100

    Added Procfile (and needed composer lock)

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # Adminer
 
 **Adminer** is a full-featured database management tool written in PHP. It consists of a single file ready to deploy
-to the target server. **Adminer Editor** offers data manipulation for end-users.
+to the target server.
 
-https://www.adminer.org/
-
-- **Supports:** MySQL, MariaDB, PostgreSQL, CockroachDB, SQLite, MS SQL, Oracle
-- **Plugins for:** Elasticsearch, SimpleDB, MongoDB, Firebird, ClickHouse
-- **Requirements:** PHP 5.3+
+https://www.adminer.org
 
 ## Screenshot
 ![screenshot](https://www.adminer.org/static/screenshots/table.png)
--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
 function adminer_object() {
-    // required to run any plugin
     include_once "./plugins/plugin.php";
 
-    // autoloader
     foreach (glob("plugins/*.php") as $filename) {
         include_once "./$filename";
     }
 
-    // enable extra drivers just by including them
-    //~ include "./plugins/drivers/simpledb.php";
-
     $plugins = array(
-        // specify enabled plugins here
         new AdminerCodemirror(),
     );
 
@@ -20,5 +14,4 @@ function adminer_object() {
     return new AdminerPlugin($plugins);
 }
 
-// include original Adminer or Adminer Editor
 include "./adminer.php";
